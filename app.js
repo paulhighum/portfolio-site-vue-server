4d2962cbefa822bc7d8b6e3bf66bcd36fc08509e
Paul Highum <phigh07@gmail.com>
2018-04-03 14:04:58 -0600

    npm i nodemailer

--- app.js
+++ app.js
@@ -2,10 +2,33 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const cors = require("cors")
+const mailer = require('./mailer')
 
 app.use(bodyParser.json())
 app.use(cors())
 
+
+app.post("/send", (req, res) => {
+  const message = {
+    from: process.env.FROM_MAIL,
+    to: process.env.TO_MAIL,
+    subject: "Site Contact Form",
+    message: `From: ${req.body.name} \n Email: ${req.body.email} \n Sent: ${new Date()} \n Message: ${req.body.message}`
+  }
+  mailer
+    .sendMessage(message)
+    .then(() => {
+      res.json({
+        message: "Email Sent"
+      })
+    })
+    .catch(error => {
+      res.status(500)
+      res.json({
+        error: error
+      })
+    })
+})
 
 app.use((req, res, next) => {
   const err = new Error("Not Found")
