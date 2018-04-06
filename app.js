0541b9d34122d3a2e155a6d997b5edac3f4c38bb
Paul Highum <phigh07@gmail.com>
2018-04-06 15:03:30 -0600

    remove cors

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
-const cors = require('cors')
 
 require('dotenv').config()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false}))
-app.use(cors())
 
 const mailer = require('./mailer')
 
